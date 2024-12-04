02119c7d7f5be40b3d7a88ae24f52d06a2bcda69
Corgo <94418843+TeamCorgo@users.noreply.github.com>
2024-12-04 15:20:29 -0700

    Added storage, removed xml

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ sudo docker rm Website > /dev/null 2>&1
 
 sudo docker build -t website .
 
-sudo docker run -d --name Website --restart=always  -p 81:80 website
+sudo docker run -d --name Website --restart=always  -p 81:80 website -v Website_Logs:/etc/nginx/logs
